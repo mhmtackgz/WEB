60b43fe1fd90b849dab356ff02bbf6ff7cfbefc1
Mehmet Açıkgöz <102829820+mhmtackgz@users.noreply.github.com>
2023-11-29 02:21:05 +0300

    Add files via upload

---
+++ LOGIN/adminlogin.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="adminlogin.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+
+</head>
+<body background="img/vector-cars-collection-vehicles-flat-style_1284-44161.avif">
+    
+<div class="box">
+	<form>
+		<span class="text-center"> Admin Giriş</span>
+	<div class="input-container">
+		<input type="text" required=""/>
+		<label>Kullanıcı Adı</label>		
+	</div>
+	<div class="input-container">		
+		<input type="password" required=""/>
+		<label>Şifre</label>
+	</div>
+    <button type="button" class="btn btn-primary">Giriş Yap</button>
+
+</form>	
+</div>
+   
+
+</div>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  
+</html>
